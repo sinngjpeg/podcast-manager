4d849e2cda4afef29c68cf5af9cf449ad04e8c01
Ingrid Silva <ingridsilvatec@outlook.com>
2025-07-10 19:06:32 -0300

    create server http

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-console.log("Hello World!")
+import * as http from "http";
+
+const server = http.createServer((
+    request: http.IncomingMessage,
+    responde: http.ServerResponse) => { }
+);
+
+const port = process.env.PORT;
+
+server.listen(port, () => {
+    console.log(`servidor iniciado na porta ${port}`);
+});
+
