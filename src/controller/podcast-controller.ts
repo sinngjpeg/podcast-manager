b5e91bada7f7390eb4677af605ba0dc31dba776e
Ingrid Silva <ingridsilvatec@outlook.com>
2025-07-12 10:44:53 -0300

    change controller

---
+++ src/controller/podcast-controller.ts
@@ -0,0 +1,13 @@
+import { IncomingMessage, ServerResponse } from "http";
+import {serviceListEpisodes} from '../services/list-episodes-services';
+
+export const getListEpisodes = async (
+    request: IncomingMessage,
+    response: ServerResponse
+) => {
+    const content = await serviceListEpisodes();
+
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(
+        JSON.stringify(content));
+};
